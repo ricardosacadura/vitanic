9396cbb99abc5947a58e071ff8038bf7da40b66f
ricardosacadura <ricardorochasacadura@gmail.com>
2022-03-31 17:47:15 +0100

    filtering to merge 2.0

--- script.js
+++ script.js
@@ -57,8 +57,6 @@ d3.csv(vitanic, function (data) {
     .style("opacity", "0.8");
 
     
-    /*  if (m == true) { return "0.8" }
-      else { return "0.1" };*/
 
 
   var cross = d3.symbol().type(d3.symbolCross).size(35);
